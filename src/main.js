fe2285d2505d7e5e7bce875b6e1f6a533eccb9a9
Daniel Martinez <dandro.dsign@gmail.com>
2019-08-19 20:27:30 +1000

    Add font size and read mode tool

--- src/main.js
+++ src/main.js
@@ -7,3 +7,24 @@ document
     const main = document.querySelector('.main');
     main.classList.toggle(activeClass);
   });
+
+document
+  .querySelector('#adjustFontSize')
+  .addEventListener('change',
+    e => {
+      const nextFontSize = e.target.value;
+      document
+        .querySelectorAll('.paragraph')
+        .forEach(
+          element => {
+            console.log(element);
+            element.style.fontSize = `${nextFontSize}px`;
+            console.log(nextFontSize);
+          });
+    });
+
+document
+  .querySelector('#adjustReadMode')
+  .addEventListener('change', () => {
+    document.body.classList.toggle('readMode');
+  });
